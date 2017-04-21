28a64fb9eef41ef2e9071930bec5a8c818991a2c
Kirill Topolyan <kirill@topolyan.com>
2017-04-21 21:42:46 +0300

    p-tourist

--- app/assets/javascripts/spa-demo/app.router.js
+++ app/assets/javascripts/spa-demo/app.router.js
@@ -18,13 +18,9 @@
       url: "/subjects",
       templateUrl: APP_CONFIG.subjects_page_html
     })
-    .state("accountSignup",{
-      url: "/signup",
-      templateUrl: APP_CONFIG.signup_page_html
-    })
-    .state("authn",{ 
-      url: "/authn",
-      templateUrl: APP_CONFIG.authn_page_html
+    .state("trip-logs",{
+      url: "/trip-logs",
+      templateUrl: APP_CONFIG.trip_logs_html
     })
     .state("images",{
       url: "/images/:id",
@@ -34,17 +30,18 @@
       url: "/things/:id",
       templateUrl: APP_CONFIG.things_page_html
     })
-
+    .state("accountSignup",{
+      url: "/signup",
+      templateUrl: APP_CONFIG.signup_page_html
+    })
+    .state("authn",{ 
+      url: "/authn",
+      templateUrl: APP_CONFIG.authn_page_html
+    })
     .state("foos",{
       url: "/foos",
       templateUrl: APP_CONFIG.foos_page_html
     })
-
-    .state("trip-logs",{
-      url: "/trip-logs",
-      templateUrl: APP_CONFIG.trip_logs_html
-    })
-
     .state("about",{
       url: "/about",
       templateUrl: APP_CONFIG.about_page_html
